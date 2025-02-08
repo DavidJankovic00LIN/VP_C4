9836846aed3a0a7bd490715e896ed2db4e1d4537
DavidJankovic00LIN <davidubuntu157@gmail.com>
2025-02-08 13:50:20 +0100

    poceo pisanje brama

---
+++ vp/bram.hpp
@@ -0,0 +1,25 @@
+#ifndef _BRAM_HPP_
+#define _BRAM_HPP_
+
+#include<sysyemc>
+#include<iostream>
+#include<vector>
+#include <tlm>
+#include <tlm_utils/simple_target_socket.h>
+
+class Bram: public sc_core::sc_module
+{
+	public:
+		Bram(sc_core::sc_module_name name);
+		~Bram();
+
+		tlm_utils::simple_target_socket<Bram>bram_socket_1;
+		tlm_utils::simple_target_socket<Bram>bram_socket_2;
+
+	protected:
+		void b_transport(pl_t &,sc_core::sc_time &);
+		std::std::vector<unsigned char> mem;
+
+};
+
+#endif
---
+++ vp/defines.hpp
@@ -0,0 +1,14 @@
+#ifndef TYPEDEFS_HPP
+#define TYPEDEFS_HPP
+
+#define SC_INCLUE_FX
+#include<tlm>
+#include<sysc/datatypes/fx/sc_fixed.h>
+
+typedef tlm:tlm_base_protocol_types::tlm_payload_type pl_t;
+typedef tlm:tlm_base_protocol_types::tlm_phase_type ph_t;
+
+//bram size -to be determined-
+#define BRAM_SIZE 9999 //9999 is a placeholder
+
+#endif// TYPEDEFS_HPP
